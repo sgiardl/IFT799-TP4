e3b347d83906625591d1196ef383637d04725480
sgiardl <sgiardleroux@gmail.com>
2021-12-03 15:42:17 -0500

    Added package imports

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ Simon Giard-Leroux
 """
 
 from os.path import join
+from scipy.stats import pearsonr
+from sklearn.metrics import mean_absolute_error, mean_squared_error  # add ROOT
 
 from src.DataManager import DataManager
+
 
 if __name__ == '__main__':
     data_manager = DataManager(file_path=join('data', 'MovieLens-100k', 'ml-100k'))
